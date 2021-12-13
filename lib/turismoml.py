111c8b06f5516ac2d44ba6c2663abdca64e26bb6
Juan Pedro Pérez Alcántara <jp.perez.alcantara@gmail.com>
2021-12-14 00:58:39 +0100

    wip

--- lib/turismoml.py
+++ lib/turismoml.py
@@ -1,2 +1,25 @@
-def module():
-  print("Module")
+# --------------------------------------
+#
+# Modelo geográfico para la ampliación de alojamientos turísticos
+#
+# --------------------------------------
+import pandas as pd
+
+# --------------------------------------
+#
+# Algunas configuraciones
+#
+# --------------------------------------
+# D: cambiar develop por main
+baseUrl = "https://raw.githubusercontent.com/malkab/turismo_geo_ml/develop/"
+
+# --------------------------------------
+#
+# Carga de los datos de encuesta. Devuelve un DataFrame Pandas a partir de los
+# datos leidos del CSV.
+#
+# --------------------------------------
+def cargaEncuestas():
+  # D: develop por main
+  url = baseUrl + "datos/encuestas.csv"
+  return pd.read_csv(url)
